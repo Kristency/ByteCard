4c4826220796ef40f8c26d463b13ff19e81de1fe
iamakshatjain <32021987+iamakshatjain@users.noreply.github.com>
2019-04-13 13:05:40 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,22 @@
+var express 		= require("express");
+var app 			= express();
+var bodyParser 		= require("body-parser");
+var cors 			= require("cors");
+var dotenv 			= require("dotenv");
+var mongoose 		= require("mongoose");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+dotenv.config();
+mongoose.connect(process.env.LDATABASEURL,{useNewUrlParser:true});
+
+app.get("/",(req,res) => {
+	res.send("Working fine sir");
+});
+
+
+app.listen(process.env.PORT || 3000,process.env.IP,() => {
+	console.log("I am at your service sir...");
+});
+
+
