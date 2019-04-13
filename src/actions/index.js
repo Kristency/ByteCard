e66e148d3d7e67a2c26536359556db11e19a303b
Kristency <nandisubhajit666@gmail.com>
2019-04-13 19:56:19 +0530

    render login button

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,14 @@
-export const signIn = () => {
+import { SIGN_IN, SIGN_OUT } from './types';
+
+export const signIn = (currentUser) => {
 	return {
-		type: 'SIGN_IN'
+		type: SIGN_IN,
+		payload: currentUser
 	};
 };
 
 export const signOut = () => {
 	return {
-		type: 'SIGN_OUT'
+		type: SIGN_OUT
 	};
 };
